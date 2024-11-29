9fbce83046d05be38cb5399653a9752e949ad6e2
Nadezhda Zolotareva <nadzolot1@roregroup.net>
2024-11-29 12:28:15 +0300

    fix rask 2

--- 2_task.py
+++ 2_task.py
@@ -1,47 +1,46 @@
 class Fraction:
 
-   def __init__(self,number_numerator, number_denominator = None):
+     def __init__(self,number_numerator, number_denominator=None):
         
-        if type(number_numerator) == str:
-            result = number_numerator.split('/')
-            self.number_numerator = int(result[0])
-            self.number_denominator = int(result[1])
-        else:
-            self.number_numerator = number_numerator
-            self.number_denominator = number_denominator
+          if isinstance(number_numerator, str):
+               result = number_numerator.split('/')
+               self.number_numerator = int(result[0])
+               self.number_denominator = int(result[1])
+          else:
+               self.number_numerator = number_numerator
+               self.number_denominator = number_denominator
 
-        self.__fraction_reduction()
-        # self.numerator()
-        # self.denominator()
+          self.__fraction_reduction()
+
     
-   def __fraction_reduction(self):
-        a = self.number_numerator
-        b = self.number_denominator
-        while b!= 0:
-            a, b = b, a % b
+     def __fraction_reduction(self):
+          a = self.number_numerator
+          b = self.number_denominator
+          while b!= 0:
+               a, b = b, a % b
         
         # для корректного вывода __repr__ перевод в формат int
-        self.number_numerator = int(self.number_numerator / a)  
-        self.number_denominator = int(self.number_denominator / a)
+          self.number_numerator = int(self.number_numerator / a)  
+          self.number_denominator = int(self.number_denominator / a)
 
-   def numerator(self, num = None):
-        if num:
-            self.number_numerator = num
-            self.__fraction_reduction()
-        return self.number_numerator
+     def numerator(self, num = None):
+          if num:
+               self.number_numerator = num
+               self.__fraction_reduction()
+          return self.number_numerator
 
-   def denominator(self, num = None):
-        if num:
-            self.number_denominator = num
-            self.__fraction_reduction()
-        return self.number_denominator
+     def denominator(self, num = None):
+          if num:
+               self.number_denominator = num
+               self.__fraction_reduction()
+          return self.number_denominator
 
 
-   def __str__(self) -> str:
-        return str(self.number_numerator) + "/" + str(self.number_denominator)
+     def __str__(self) -> str:
+          return f'({str(self.number_numerator)}/{str(self.number_denominator)})'
    
-   def __repr__(self):
-        return "Fraction (" + str(self.number_numerator)+ ", " + str(self.number_denominator) + ")"
+     def __repr__(self):
+          return f'Fraction({str(self.number_numerator)}, {str(self.number_denominator)})'
 
 
 
